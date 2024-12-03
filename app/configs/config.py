47c559e35f9b2fdd5693a1163a9b9187edf62b28
Thoriq Afif <mthoriqafif01@gmail.com>
2024-12-03 19:34:33 +0700

    initial register

--- app/configs/config.py
+++ app/configs/config.py
@@ -9,11 +9,12 @@ class Config:
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=1)
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SUPABASE_DB_URL')
+    
     # MAIL SERVICE CONFIGURATION
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'clarisapp21@gmail.com'
-    MAIL_PASSWORD = 'acdp ofua bjco xdrh'  
-    MAIL_DEFAULT_SENDER = 'clarisapp21@gmail.com'
+    MAIL_USERNAME = 'greenifyme13@gmail.com'
+    MAIL_PASSWORD = 'greenify241124'  
+    MAIL_DEFAULT_SENDER = ('Greenify', 'greenifyme13@gmail.com')
     
